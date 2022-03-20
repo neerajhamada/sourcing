94d1809cdc1f9c81de2ce8766f14ad68d1935e5f
neerajhamada <neerajh231@gmail.com>
2022-03-20 21:29:22 +0530

    Search Icon

--- src/Sourcing.js
+++ src/Sourcing.js
@@ -46,13 +46,13 @@ export default function Sourcing() {
             {/* <Form /> */}
             
         </div>
-        <div className="input-group rounded mb-2">
+        <div className="input-group rounded mb-3 ">
             <input type="search" className="form-control rounded" placeholder="Search by Lab or Selection Status" aria-label="Search" aria-describedby="search-addon"
             onChange={(event)=>{
                 setSearchTerm(event.target.value);
             }}/>
-        <span class="input-group-text border-0" id="search-addon">
-            <i class="fas fa-search"></i>
+        <span className="input-group-text border-0" id="search-addon">
+            <i className="bi bi-search"></i>
         </span>
         </div>
         <table style={{fontSize:'12px'}} className="table table-sm table-responsive-sm table-dark table-bordered table-hover text-center shadow">
